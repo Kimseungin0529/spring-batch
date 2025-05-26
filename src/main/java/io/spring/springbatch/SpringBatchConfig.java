d37056a29d70ad45a166099349d6650b6d79dccb
Kimseungin0529 <whffkaos007@naver.com>
2025-05-26 16:22:13 +0900

    스프링배치-초기화설정

--- src/main/java/io/spring/springbatch/SpringBatchConfig.java
+++ src/main/java/io/spring/springbatch/SpringBatchConfig.java
@@ -20,36 +20,56 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class HelloSpringBatchConfig {
-
-    private final String JOB_NAME = "helloJob";
-    private final String STEP_NAME = "helloStep";
+public class SpringBatchConfig {
 
 
     @Bean
-    public Job helloJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new JobBuilder(JOB_NAME, jobRepository)
-                .start(testStep(jobRepository, transactionManager)) // step 설정
+    public Job job1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("job1", jobRepository)
+                .start(step1(jobRepository, transactionManager)) // step 설정
+                .next(step2(jobRepository, transactionManager))
                 .build();
     }
 
 
     @Bean
     @JobScope
-    public Step testStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder(STEP_NAME, jobRepository)
-                .tasklet(testTasklet(), transactionManager)
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(testTasklet1(), transactionManager)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet(testTasklet2(), transactionManager)
                 .build();
     }
 
     @Bean
     @StepScope
-    public Tasklet testTasklet() {
+    public Tasklet testTasklet1() {
         return new Tasklet() {
             @Override
             public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                 log.info("=================================");
-                log.info("Spring Batch Test Success");
+                log.info("Spring Batch : job1-> step1 Success");
+                log.info("=================================");
+                return RepeatStatus.FINISHED; // 작업에 대한 Status 명시
+            }
+        };
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet testTasklet2() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                log.info("=================================");
+                log.info("Spring Batch : job1-> step2 Success");
                 log.info("=================================");
                 return RepeatStatus.FINISHED; // 작업에 대한 Status 명시
             }
